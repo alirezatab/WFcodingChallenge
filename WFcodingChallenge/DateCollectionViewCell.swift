00c939c145bc218ecc1e6c39a84ef97aa1aa9e50
alirezatab <alirezatabrizi10@gmail.com>
2017-02-16 17:19:16 -0800

    updated the reservation views so it is cleaner and placed them inside containerViwes

---
+++ WFcodingChallenge/DateCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DateCollectionViewCell.swift
+//  WFcodingChallenge
+//
+//  Created by ALIREZA TABRIZI on 2/16/17.
+//  Copyright © 2017 AR-T.com, Inc. All rights reserved.
+//
+
+///cellIdentifier DateCollectionViewCell
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var weekdayLabel: UILabel!
+    @IBOutlet weak var dayOfMonthNumberLabel: UILabel!
+    
+    
+}
