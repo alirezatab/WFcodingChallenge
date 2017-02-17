e0b82653a0bde9eba63428d36b33617bf90728c6
alirezatab <alirezatabrizi10@gmail.com>
2017-02-16 19:39:31 -0800

    was able to get all the days in the month in the form of int, as well as the current month + all the possible days in a month, next: need to assig it to the correct label and display it

---
+++ WFcodingChallenge/DateCollectionViewDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  DateCollectionViewDataSource.swift
+//  WFcodingChallenge
+//
+//  Created by ALIREZA TABRIZI on 2/16/17.
+//  Copyright © 2017 AR-T.com, Inc. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewDataSource: NSObject {
+
+}
