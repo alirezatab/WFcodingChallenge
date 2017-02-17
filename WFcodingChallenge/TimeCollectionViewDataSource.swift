d28f65023351fd507cb5aa4164ee791c36a0c22a
alirezatab <alirezatabrizi10@gmail.com>
2017-02-17 01:04:02 -0800

    implemented TimeCollectionViewDataSource

--- WFcodingChallenge/TimeCollectionViewDataSource.swift
+++ WFcodingChallenge/TimeCollectionViewDataSource.swift
@@ -10,6 +10,23 @@ import UIKit
 
 private let reuseIdentifier = "TimeCollectionViewCell"
 
-class TimeCollectionViewDataSource: NSObject {
+class TimeCollectionViewDataSource: NSObject, UICollectionViewDataSource {
+    
+    private let dataSource = TimeModel()
+    private let dateFormatter = DateFormatter()
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.getReservationTimeIntervals().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TimeCollectionViewCell
+        
+        let times = dataSource.getReservationTimeIntervals()
+        
+        cell.timeLabel.text = dateFormatter.string(from: times[indexPath.row])
+        
+        return cell
+    }
 }
